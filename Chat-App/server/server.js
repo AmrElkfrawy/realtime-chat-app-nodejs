317dacd4e323b60f1d101c4d204ed554be83b5de
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2023-09-27 07:02:01 +0300

    Set up Express.js and Socket.IO

--- Chat-App/server/server.js
+++ Chat-App/server/server.js
@@ -1,15 +1,27 @@
+const http = require('http');
 const path = require('path');
+
 const express = require('express');
 const dotenv = require('dotenv');
+const socketIO = require('socket.io');
 
 dotenv.config({ path: './.env' });
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '/../public');
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('A new user connected');
+  socket.on('disconnect', (socket) => {
+    console.log('User was disconnected');
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Server listening on port ', port);
+server.listen(port, () => {
+  console.log('Server listening on port', port);
 });
