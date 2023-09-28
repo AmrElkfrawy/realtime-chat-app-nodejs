e9c1ea4ed2d006d315c8773c14317513432d35a1
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2023-09-28 06:22:16 +0300

    Improved style of the app

--- public/js/index.js
+++ public/js/index.js
@@ -10,26 +10,58 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   const formattedTime = moment(message.createdAt).format('LT');
-  const li = document.createElement('li');
-  li.innerText = `${message.from} (${formattedTime}): ${message.text} `;
+  const template = document.querySelector('#message-template').innerHTML;
+  const html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime,
+  });
+
+  const div = document.createElement('div');
+  div.innerHTML = html;
 
   const messagesList = document.querySelector('#messages');
-  messagesList.appendChild(li);
+  messagesList.appendChild(div);
+
+  // const formattedTime = moment(message.createdAt).format('LT');
+  // const li = document.createElement('li');
+  // li.innerText = `${message.from} (${formattedTime}): ${message.text} `;
+
+  // const messagesList = document.querySelector('#messages');
+  // messagesList.appendChild(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   const formattedTime = moment(message.createdAt).format('LT');
-  const li = document.createElement('li');
-  li.innerText = `${message.from} (${formattedTime}): `;
 
-  const a = document.createElement('a');
-  a.setAttribute('target', '_blank');
-  a.setAttribute('href', message.url);
-  a.innerText = 'My current location';
-  li.appendChild(a);
+  const template = document.querySelector(
+    '#location-message-template'
+  ).innerHTML;
+
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime,
+  });
+
+  const div = document.createElement('div');
+  div.innerHTML = html;
 
   const messagesList = document.querySelector('#messages');
-  messagesList.appendChild(li);
+  messagesList.appendChild(div);
+
+  // const formattedTime = moment(message.createdAt).format('LT');
+  // const li = document.createElement('li');
+  // li.innerText = `${message.from} (${formattedTime}): `;
+
+  // const a = document.createElement('a');
+  // a.setAttribute('target', '_blank');
+  // a.setAttribute('href', message.url);
+  // a.innerText = 'My current location';
+  // li.appendChild(a);
+
+  // const messagesList = document.querySelector('#messages');
+  // messagesList.appendChild(li);
 });
 
 document.querySelector('#submit-btn').addEventListener('click', function (e) {
